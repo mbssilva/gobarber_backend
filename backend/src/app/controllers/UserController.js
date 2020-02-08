b4518c8bd2876b067f878a54d840176308e63e31
mbssilva <mbernardi.s@outlook.com>
2020-02-08 17:33:58 -0300

    Validação do body das requisições baseado nos dados esperados usando a lib Yup

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,8 +1,21 @@
+// Importando as bibliotecas
+import * as Yup from 'yup';
+
 // Importando model do controller
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({error: 'Validation failed'});
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -15,6 +28,26 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string().min(6).when('oldPassword', (oldPassword, field) =>
+        oldPassword ? field.required() : field
+      ),
+      passwordConfirmation: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+      teste: Yup.string().when('name', (name, atual) => {
+        console.log(name);
+        if (name) return atual;
+      })
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({error: 'Validation failed'});
+    }
+
     const id = req.userId;
     const {email, oldPassword} = req.body;
 
